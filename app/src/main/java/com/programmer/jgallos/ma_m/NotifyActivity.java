d9c603b926da70992cf89eec2ca4f58bc14e33fb
jgallos <hsitechnologies@gmail.com>
2019-05-08 21:31:23 +0800

    08/05/2019 9:31pm - Notification system changes

--- app/src/main/java/com/programmer/jgallos/ma_m/NotifyActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_m/NotifyActivity.java
@@ -9,11 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.instacart.library.truetime.TrueTimeRx;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class NotifyActivity extends AppCompatActivity {
 
@@ -41,16 +50,20 @@ public class NotifyActivity extends AppCompatActivity {
 
                 final DatabaseReference newNotif = mNotifDatabase.push();
                 String stringNotif = null;
+                String timeHolder = null;
 
                 stringNotif = notifContentEdit.getText().toString().trim();
 
+                timeHolder = updateTime(1);
                 newNotif.child("nnotif").setValue("Notification from Manager: " + stringNotif);
-                newNotif.child("ndate").setValue("x");
-                newNotif.child("ntime").setValue("x").addOnCompleteListener(new OnCompleteListener<Void>() {
+                newNotif.child("ndate").setValue(timeHolder);
+                timeHolder = updateTime(2);
+                newNotif.child("ntime").setValue(timeHolder).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-
+                            Toast.makeText(NotifyActivity.this,"Notification sent!", Toast.LENGTH_SHORT).show();
+                            finish();
 
                         }
                     }
@@ -69,5 +82,32 @@ public class NotifyActivity extends AppCompatActivity {
             }
         }); */
     }
+
+
+    private String updateTime(int flag) {
+        TextView textTime = (TextView) findViewById(R.id.textViewNTime);
+
+        if (!TrueTimeRx.isInitialized()) {
+            Toast.makeText(NotifyActivity.this, "TrueTime is not yet initialized.", Toast.LENGTH_SHORT).show();
+            return " ";
+        }
+
+        Date trueTime = TrueTimeRx.now();
+        if (flag == 1) {
+            textTime.setText(getString(R.string.tt_time_gmt, _formatDate(trueTime, "yyyy-MM-dd", TimeZone.getTimeZone("GMT+08:00"))));
+
+        } else if (flag==2) {
+            textTime.setText(getString(R.string.tt_time_gmt, _formatDate(trueTime, "HH:mm:ss", TimeZone.getTimeZone("GMT+08:00"))));
+        }
+        return textTime.getText().toString();
+    }
+
+    private String _formatDate(Date date, String pattern, TimeZone timeZone) {
+        DateFormat format = new SimpleDateFormat(pattern, Locale.ENGLISH);
+        format.setTimeZone(timeZone);
+        return format.format(date);
+    }
+
+
 
 }
