504cb44060d00fa39c674711ad439e7294f36d4b
jgallos <hsitechnologies@gmail.com>
2019-05-09 16:50:21 +0800

    09/05/2019 4:50pm - register error fixed

--- app/src/main/java/com/programmer/jgallos/ma_m/RegisterActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_m/RegisterActivity.java
@@ -93,14 +93,7 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
 }
