2a0ef714d276913795e31363a58935ce4ab9a7cf
jgallos <hsitechnologies@gmail.com>
2019-05-15 05:22:08 +0800

    15/05/2019 5:21am - Added attendance search

--- app/src/main/java/com/programmer/jgallos/ma_m/AttendanceActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_m/AttendanceActivity.java
@@ -9,7 +9,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -20,6 +24,11 @@ public class AttendanceActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     String subjectBuffer = null;
     private DatabaseReference mDatabase;
+    String firstTwoChars = "";
+    String searchString = "";
+
+
+    private Button searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,88 @@ public class AttendanceActivity extends AppCompatActivity {
         subjectBuffer = getIntent().getExtras().getString("subjectReviewed");
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child(subjectBuffer + "_attendance");
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerMonths);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.months, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        searchBtn = (Button)findViewById(R.id.buttonSearch);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner spinnerContent = (Spinner)findViewById(R.id.spinnerMonths);
+                final String sMonths = spinnerContent.getSelectedItem().toString();
+
+                if (sMonths.equals("January")){
+                    searchString = "01";
+                } else if (sMonths.equals("February")){
+                    searchString = "02";
+                } else if (sMonths.equals("March")) {
+                    searchString = "03";
+                } else if (sMonths.equals("April")) {
+                    searchString = "04";
+                } else if (sMonths.equals("May")) {
+                    searchString = "05";
+                } else if (sMonths.equals("June")) {
+                    searchString = "06";
+                } else  if (sMonths.equals("July")) {
+                    searchString = "07";
+                } else if (sMonths.equals("August")) {
+                    searchString = "08";
+                } else if (sMonths.equals("September")) {
+                    searchString = "09";
+                } else if (sMonths.equals("October")) {
+                    searchString = "10";
+                } else if (sMonths.equals("November")) {
+                    searchString = "11";
+                } else if (sMonths.equals("December")) {
+                    searchString = "12";
+                }
+
+
+                FirebaseRecyclerAdapter<AttendanceRecord, AttendanceViewHolder> FBRA = new FirebaseRecyclerAdapter<AttendanceRecord, AttendanceViewHolder>(
+                        AttendanceRecord.class,
+                        R.layout.attendance_list,
+                        AttendanceViewHolder.class,
+                        mDatabase
+                ) {
+                    @Override
+                    protected void populateViewHolder(AttendanceViewHolder viewHolder, AttendanceRecord model, int position) {
+
+                        if (model.getDate().length() > 2)
+                        {
+                            firstTwoChars = model.getDate().substring(5,7 );
+                        }
+                        else
+                        {
+                            firstTwoChars = model.getDate();
+                        }
+
+                        //Toast.makeText(AttendanceActivity.this,firstTwoChars,Toast.LENGTH_LONG).show();
+
+                        if (firstTwoChars.equals(searchString)) {
+                            viewHolder.setDate(model.getDate());
+                            viewHolder.setSignin(model.getSignin());
+                            viewHolder.setSignout(model.getSignout());
+                            viewHolder.setName(model.getName());
+
+                            viewHolder.mView.setVisibility(View.VISIBLE);
+                        } else {
+                            viewHolder.mView.setVisibility(View.GONE);
+                            viewHolder.mView.setLayoutParams(new RecyclerView.LayoutParams(0,0));
+                        }
+
+                    }
+                };
+                recyclerView.setAdapter(FBRA);
+
+            }
+        });
+
 
     }
 
